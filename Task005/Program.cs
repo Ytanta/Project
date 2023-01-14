7649e6208dfe20cd36fe63db7e863b0e241c0c48
Igor <igor>
2023-01-14 04:52:59 +0300

    Добавили Task007

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -4,7 +4,7 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> " -2, -1, 0, 1, 2"
 
-Console.WriteLine("Введите числоЖ ");
+Console.WriteLine("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 if (num > 0)
 {
---
+++ Task007/Program.cs
@@ -0,0 +1,19 @@
+﻿// 7. Напигите программу, которая принимает на вход
+// трехзначное число и на выходе показывает последнюю цифру 
+// этого числа.
+//    456 -> 6
+//    782 -> 2
+//    918 -> 8
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if 
+(99 < num & num<1000)
+{
+    int res = num % 10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
