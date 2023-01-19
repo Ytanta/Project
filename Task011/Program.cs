dadcca7671221ffa3c6c3f38d207a2957d8bd05b
Igor <igor>
2023-01-19 10:36:32 +0300

    Task 011

---
+++ Task011/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+
+int doublenum = Doublenum(number);
+Console.WriteLine($"Без второго числа -> {doublenum}");
+
+int Doublenum(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
