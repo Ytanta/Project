3a31d05f79818eab5d9b9493ef4e1df0835203bc
Igor <igor>
2023-01-26 15:46:42 +0300

    Task028

---
+++ Task028/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0)
+{
+    Console.WriteLine("Некорректное число");
+}
+else
+{
+    int factorial = Factorial(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} = {factorial}");
+}
+int Factorial(int num)
+{
+    int composition = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            composition = sum * i;
+        }
+    }
+    return composition;
+}
