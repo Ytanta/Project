68b1c3cfd78c13b91a15b6b2a23d580fbd7b1b06
Igor <igor>
2023-02-02 17:10:40 +0300

    task042

---
+++ Task042/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число:");
+int number; 
+if(int.TryParse(Console.ReadLine(), out number))
+{ 
+int result = NumBinary(number);
+Console.WriteLine($"{number} -> {result}");
+}
+else Console.WriteLine("Некорректное число");
+
+
+int NumBinary(int num)
+{
+    int numbrtBin = 0;
+    int count = 1;
+    while (num > 0)
+    {
+        numbrtBin = numbrtBin + (num % 2 * count);
+        num /= 2;
+        count *= 10;
+    }
+    return numbrtBin;
+}
