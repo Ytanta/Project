5ab7c641b12685c67665e8314a83f625afd59f45
Igor <igor>
2023-02-07 03:50:59 +0300

    task051

--- Task051/Program.cs
+++ Task051/Program.cs
@@ -6,3 +6,50 @@
 // 5 9 2 3
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] CreateArraeRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+ 
+    Console.Write("[");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4}");
+        else Console.Write($"{matrix[i,j], 4}");
+    }
+    Console.WriteLine("]");
+    }
+}
+int SumDiagonale(int[,] matrix)
+{
+    int result=0;
+    int size = matrix.GetLength(0);
+    if(matrix.GetLength(0) > matrix.GetLength(1))
+    size = matrix.GetLength(1);
+    for (int i = 0; i < size; i++)
+    {
+        result += matrix[i,i];
+    }
+    return result;
+}
+
+int[,] array2D = CreateArraeRndInt(4,3, -10,10);
+PrintMatrix(array2D);
+int result = SumDiagonale(array2D);
+Console.WriteLine(result);
