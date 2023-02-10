c635f870dccb58d94a1bab77051d1800a50a62c3
Igor <igor>
2023-02-10 04:03:34 +0300

    task062

---
+++ Task060/Program.cs
@@ -0,0 +1,74 @@
+﻿// *Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+// int[,,] CreateMatrix3D(int row, int col, int dep, int min, int max)
+// {
+//     int[,,] matrix = new int[row, col, dep];
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrix.GetLength(2); k++)
+//             {
+//                 matrix[i, j, k] = rnd.Next(min, max + 1);
+
+//             }
+//         }
+//     }
+//     return matrix;
+// }
+
+// int[,,] NnrepeatingNumber(int[,,] matrix3d, int min, int max)
+// {
+//     Random rnd = new Random();
+//     for (int n = 0; n < matrix3d.GetLength(0); n++)
+//     {
+
+//         for (int m = 0; m < matrix3d.GetLength(1); m++)
+//         {
+
+//             for (int l = 0; l < matrix3d.GetLength(2) -1; l++)
+//             {
+
+//                 for (int i = 0; i < matrix3d.GetLength(0); i++)
+//                 {
+//                     for (int j = 0; j < matrix3d.GetLength(1); j++)
+//                     {
+//                         for (int k = 0; k < matrix3d.GetLength(2); k++)
+//                         {
+//                             if (matrix3d[n, m, l+1] == matrix3d[i, j, k])
+//                             {
+//                                 matrix3d[n, m, l] = rnd.Next(min, max + 1);
+
+//                             }
+
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return matrix3d;
+// }
+// void PrintMatrix(int[,,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write("|");
+//             for (int k = 0; k < matrix.GetLength(2); k++) { Console.Write($"{matrix[i, j, k],1}({i},{j},{k})|"); }
+//             Console.WriteLine();
+//         }
+//     }
+// }
+// int[,,] matrix3D = CreateMatrix3D(2, 2, 2, 10, 18);
+
+// PrintMatrix(matrix3D);
+// Console.WriteLine();
+// matrix3D = NnrepeatingNumber(matrix3D, 10,18);
+// PrintMatrix(matrix3D);
---
+++ Task062/Program.cs
@@ -0,0 +1,55 @@
+﻿
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] SpiralMatrix(int rows, int columns)
+{
+int[,] Matrix = new int[rows, columns];
+int count = 1;
+int i = 0;
+int j = 0;
+
+while (count <= Matrix.GetLength(0) * Matrix.GetLength(1))
+{
+  Matrix[i, j] = count;
+  count++;
+  if (i <= j + 1 && i + j < Matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= Matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > Matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+return Matrix;
+}
+Console.WriteLine("Введите количество строк:");
+int Rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int Columns = Convert.ToInt32(Console.ReadLine());
+if (Rows==Columns)
+{
+int [,] spiralMatrix = SpiralMatrix(Rows,Columns);
+PrintMatrix(spiralMatrix);
+}
+else Console.WriteLine("Невозможно построить спиральную матрицу");
